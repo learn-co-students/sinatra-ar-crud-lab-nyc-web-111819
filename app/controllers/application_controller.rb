57ef60130c2cd64cfa872b0c3011eceafa2e2351
jasmosez <jas.schaffer@gmail.com>
2019-12-11 17:29:21 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -6,8 +6,52 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :method_override, 'true'
   end
 
-  get '/' do
+  get "/" do
+    binding.pry
   end
+
+  get "/articles" do
+    @articles = Article.all
+    erb :index
+  end
+  
+  post "/articles" do
+    @article = Article.create(params)
+    # binding.pry
+    redirect to "/articles/#{@article.id}"
+  end
+
+  get "/articles/new" do
+    erb :new
+  end
+
+  delete "/articles/:id" do
+    binding.pry
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect to "/articles"
+  end
+
+  get "/articles/:id" do
+    # binding.pry
+    @article = Article.find(params[:id])
+    erb :show
+  end
+  
+  patch "/articles/:id" do
+    binding.pry
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{@article.id}"
+  end
+  
+  get "/articles/:id/edit" do
+    # binding.pry
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
 end
